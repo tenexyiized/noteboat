bd385566c608938bae9a05ae4eb99cfeb671fbdc
abhishek chauhan <abhishek.chauhan@wizely.in>
2022-05-02 23:21:09 +0530

    use cases abstract class and result class added

---
+++ app/src/main/java/com/example/common/usecases/UseCase.kt
@@ -0,0 +1,32 @@
+package com.example.common.usecases
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import com.example.common.result.Result
+
+abstract class UseCase<in P, R>(private val coroutineDispatcher: CoroutineDispatcher) {
+
+    /** Executes the use case asynchronously and returns a [Result].
+     *
+     * @return a [Result].
+     *
+     * @param parameters the input parameters to run the use case with
+     */
+    suspend operator fun invoke(parameters: P): Result<R> {
+        return try {
+            withContext(coroutineDispatcher) {
+                execute(parameters).let {
+                    Result.Success(it)
+                }
+            }
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
+
+    /**
+     * Override this to set the code to be executed.
+     */
+    @Throws(RuntimeException::class)
+    protected abstract suspend fun execute(parameters: P): R
+}
