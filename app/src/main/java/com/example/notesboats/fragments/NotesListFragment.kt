08a996e029cfecdba9dc0e070326b061f8412576
abhishek chauhan <abhishek.chauhan@wizely.in>
2022-05-15 11:06:22 +0530

    ui added

---
+++ app/src/main/java/com/example/notesboats/fragments/NotesListFragment.kt
@@ -0,0 +1,10 @@
+package com.example.notesboats.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.notesboats.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NotesListFragment : Fragment(R.layout.fragment_notes_list){
+
+}
